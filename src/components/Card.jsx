b7896b679b9915cdcae3c5e6e753c3b2585a44d9
Atanu Chakraborty <127716168+98atanu@users.noreply.github.com>
2024-03-11 18:04:42 +0530

    Framer motion added.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
+import { IoMdClose } from "react-icons/io";
+import { motion } from "framer-motion";
 
 
-const Card = ({data}) => {
+const Card = ({data, reference}) => {
   return (
-    <div className=' relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white overflow-hidden py-10 px-8'> 
+    <motion.div drag dragConstraints={reference} whileDrag={{scale: 1.1}} dragElastic={0.1} dragTransition={{ bounceStiffness: 100, bounceDamping: 10 }}className=' relative flex-shrink-0 w-52 h-64 rounded-[45px] bg-zinc-900/90 text-white overflow-hidden py-10 px-8'> 
         <FaRegFileAlt />
         <p className='text-xm leading-tight mt-5 font-semibold'>{data.desc}</p>
         <div className="footer absolute bottom-0 w-full left-0 ">
            <div className='flex items-center justify-between py-3 px-8 mb-3'>
             <h5>{data.filesize}</h5>
             <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
-            <LuDownload size="0.7em" color='#fff' />
+              {data.close ? <IoMdClose /> : <LuDownload size="0.7em" color='#fff' /> }
             </span>
            </div>
-           <div className="tag w-full py-4 bg-green-600 flex justify-center items-center">
-            <h3 className='text-sm font-semibold'>Download Now</h3>
-           </div>
+           {data.tag.isOpen && (
+               <div className={`tag w-full py-4 ${data.tag.tagColor === "green" ? "bg-violet-900" : "bg-teal-950" } flex justify-center items-center`}>
+                   <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
+              </div>
+           )}          
         </div>  
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
 import Card from "./Card";
+import { motion } from "framer-motion";
 
 const Foreground = () => {
+  const ref = useRef(null);
   const data = [
     {
     desc: "This is the background color of the data that will be displayed",
     filesize: "0.9mb",
     close: true,
+    tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green" },
+  },
+  {
+    desc: "This is the background color of the data that will be displayed",
+    filesize: "0.9mb",
+    close: true,
     tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+  },
+  {
+    desc: "This is the background color of the data that will be displayed",
+    filesize: "0.9mb",
+    close: true,
+    tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue" },
   },
 ];
 
   return (
-    <div className="fixed z-[3] top-0 left-0 w-full h-full ">
+    <motion.div ref ={ref} className="fixed z-[3] top-0 left-0 w-full h-full flex gap-10 flex-wrap p-5">
       {data.map((item, index) => (
-        <Card data={item} />
+        <Card data={item} reference = {ref} />
       ))}
-    </div>
+    </motion.div>
   );
 };
 
